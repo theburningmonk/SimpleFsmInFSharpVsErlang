1e2c58696499f1b938a9acffedb0b274448e3dec
Yan Cui <theburningmonk@gmail.com>
2012-07-28 01:50:22 +0100

    - F# imlementation of FSM and unit tests

---
+++ FSharpExample/FSharpExample/Fsm.fs
@@ -0,0 +1,60 @@
+﻿namespace Fsm
+
+open System
+open System.Threading
+open FSharp.Control
+
+type State = | Locked of int list
+             | Open
+             | Closed
+
+type Message = | Button     of int
+               | Close      of AsyncReplyChannel<unit>
+               | GetState   of AsyncReplyChannel<State>
+
+type CodeLock (code : int list) =
+    let code = code |> List.rev
+    let doUnlock () = printfn "The code lock is now unlocked"
+    let doLock () = printfn "The code lock is now locked"
+    let doCleanUp () = printfn "The code lock is now terminated..."
+
+    let cts = new CancellationTokenSource()
+    let agent = Agent<Message>.Start((fun inbox ->
+        let rec lockedState (soFar : int list) = 
+            async {
+                let! msg = inbox.Receive()
+                match msg with
+                | Button(digit) when soFar.Length < code.Length - 1 -> return! lockedState (digit::soFar)
+                | Button(digit) when (digit::soFar) = code -> doUnlock()
+                                                              return! openState ()
+                | Button(digit) -> return! lockedState []
+                | GetState(reply) -> reply.Reply(Locked soFar)
+                                     return! lockedState soFar
+                | Close(reply) -> doCleanUp()
+                                  reply.Reply()
+                                  cts.Cancel()
+                | _ -> return! lockedState soFar
+            }
+        and openState () =
+            async {
+                try
+                    let! msg = inbox.Receive(3000)
+                    match msg with
+                    | GetState(reply)
+                        -> reply.Reply(Open)
+                           return! openState()
+                    | Close(reply) 
+                        -> doCleanUp()
+                           reply.Reply()
+                           cts.Cancel()
+                    | _ -> return! openState()
+                with
+                | :? TimeoutException -> doLock()
+                                         return! lockedState []                    
+            }
+            
+        lockedState []), cancellationToken = cts.Token)
+
+    member this.Button digit = agent.Post <| Button(digit)
+    member this.GetState () = try agent.PostAndReply(GetState, 1000) with | :? TimeoutException -> Closed
+    member this.Close () = agent.PostAndReply Close
---
+++ FSharpExample/FSharpExample/FsmTests.fs
@@ -0,0 +1,71 @@
+﻿module Fsm.Tests
+
+open System
+open System.Threading
+open Fsm
+open NUnit.Framework
+open FsUnit
+
+let getLock () = new CodeLock([1; 2; 3; 4])
+let getOpenLock () = 
+    let codelock = getLock()
+    codelock.Button 1
+    codelock.Button 2
+    codelock.Button 3
+    codelock.Button 4
+    codelock
+
+[<TestFixture>]
+type ``Given a locked FSM`` () =   
+    [<Test>]
+    member test.``when the user hasn't entered anything its state should be blank`` () =
+        let codelock = getLock()
+        codelock.GetState() |> should equal <| Locked([])
+
+    [<Test>]
+    member test.``when the user enters the first digit it remains locked`` () =
+        let codelock = getLock()
+        codelock.Button 1
+        codelock.GetState() |> should equal <| Locked([1])
+
+    [<Test>]
+    member test.``when the user enters an incorrect code then the state is reset`` () =
+        let codelock = getLock()
+        codelock.Button 1
+        codelock.Button 2
+        codelock.Button 3
+        codelock.Button 5
+        codelock.GetState() |> should equal <| Locked([])
+
+    [<Test>]
+    member test.``when the user enters the correct code then the state is changed to open`` () =
+        let codelock = getLock()
+        codelock.Button 1
+        codelock.Button 2
+        codelock.Button 3
+        codelock.Button 4
+        codelock.GetState() |> should equal Open
+
+    [<Test>]
+    member test.``when the user calls Close it should terminate the FSM`` () =
+        let codelock = getLock()
+        codelock.Close()
+        codelock.GetState() |> should equal Closed
+
+[<TestFixture>]
+type ``Given an open FSM`` () =
+    [<Test>]
+    member test.``when 3 seconds have passed it should revert to locked state`` () =
+        let codelock = getOpenLock()
+
+        // give it an half a second as safety buffer
+        Thread.Sleep(TimeSpan.FromSeconds 3.5)
+
+        codelock.GetState() |> should equal <| Locked([])
+
+    [<Test>]
+    member test.``when user calls Close it should terminate the FSM`` () =
+        let codelock = getOpenLock()
+
+        codelock.Close()
+        codelock.GetState() |> should equal Closed
